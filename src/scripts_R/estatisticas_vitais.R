62d7313c0b42d3773198a67ad2e1b7595891f202
Danilo <dnlgomes93@gmail.com>
2013-11-13 18:03:49 -0300

    boxplot, qqnorm mortalidade geral

--- src/scripts_R/estatisticas_vitais.R
+++ src/scripts_R/estatisticas_vitais.R
@@ -85,32 +85,6 @@ ob.causas.ext.2010 = read.csv("Estatisticas Vitais/Mortalidade, 1996 a 2011, pel
 ob.causas.ext.2011 = read.csv("Estatisticas Vitais/Mortalidade, 1996 a 2011, pela CID-10/Obitos por causas externas/2011.csv", header=F, sep=";")
 
 
-ob.causas.ext.2007$V1 = NULL
-ob.causas.ext.2007=data.frame(as.numeric(as.character(ob.causas.ext.2007$V2[1:225])), as.numeric(as.character(ob.causas.ext.2007$V3[1:225])) )
-ob.causas.ext.2007$V3 = as.numeric(as.character(ob.causas.ext.2007$V3[1:225]))
-colnames(ob.causas.ext.2007) <- c("Obitos_p.Residenc","Obitos_p.Ocorrenc")
-
-ob.causas.ext.2008$V1 = NULL
-ob.causas.ext.2008=data.frame(as.numeric(as.character(ob.causas.ext.2008$V2[1:225])), as.numeric(as.character(ob.causas.ext.2008$V3[1:225])) )
-ob.causas.ext.2008$V3 = as.numeric(as.character(ob.causas.ext.2008$V3[1:225]))
-colnames(ob.causas.ext.2008) <- c("Obitos_p.Residenc","Obitos_p.Ocorrenc")
-
-ob.causas.ext.2009$V1 = NULL
-ob.causas.ext.2009=data.frame(as.numeric(as.character(ob.causas.ext.2009$V2[1:225])), as.numeric(as.character(ob.causas.ext.2009$V3[1:225])) )
-ob.causas.ext.2009$V3 = as.numeric(as.character(ob.causas.ext.2009$V3[1:225]))
-colnames(ob.causas.ext.2009) <- c("Obitos_p.Residenc","Obitos_p.Ocorrenc")
-
-ob.causas.ext.2010$V1 = NULL
-ob.causas.ext.2010=data.frame(as.numeric(as.character(ob.causas.ext.2010$V2[1:225])), as.numeric(as.character(ob.causas.ext.2010$V3[1:225])) )
-ob.causas.ext.2010$V3 = as.numeric(as.character(ob.causas.ext.2010$V3[1:225]))
-colnames(ob.causas.ext.2010) <- c("Obitos_p.Residenc","Obitos_p.Ocorrenc")
-
-ob.causas.ext.2011$V1 = NULL
-ob.causas.ext.2011=data.frame(as.numeric(as.character(ob.causas.ext.2011$V2[1:225])), as.numeric(as.character(ob.causas.ext.2011$V3[1:225])) )
-ob.causas.ext.2011$V3 = as.numeric(as.character(ob.causas.ext.2011$V3[1:225]))
-colnames(ob.causas.ext.2011) <- c("Obitos_p.Residenc","Obitos_p.Ocorrenc")
-
-####################################
 
 formatar.df <- function(df, col1, col2){
   df$V1 = NULL
@@ -119,4 +93,49 @@ formatar.df <- function(df, col1, col2){
   colnames(df) <- c(col1,col2)
   return(df)
 }
+
+ob.causas.ext.2007 = formatar.df(ob.causas.ext.2007, "Obitos_p.Residenc","Obitos_p.Ocorrenc")
+ob.causas.ext.2008 = formatar.df(ob.causas.ext.2008, "Obitos_p.Residenc","Obitos_p.Ocorrenc")
+ob.causas.ext.2009 = formatar.df(ob.causas.ext.2009, "Obitos_p.Residenc","Obitos_p.Ocorrenc")
+ob.causas.ext.2010 = formatar.df(ob.causas.ext.2010, "Obitos_p.Residenc","Obitos_p.Ocorrenc")
+ob.causas.ext.2011 = formatar.df(ob.causas.ext.2011, "Obitos_p.Residenc","Obitos_p.Ocorrenc")
+
+todos = rbind(ob.causas.ext.2007, ob.causas.ext.2008, ob.causas.ext.2009, ob.causas.ext.2010, ob.causas.ext.2011)
+
+
+ob.causas.ext.resid = list(r2007 = ob.causas.ext.2007$Obitos_p.Residenc, r2008 = ob.causas.ext.2008$Obitos_p.Residenc,
+                        r2009 = ob.causas.ext.2009$Obitos_p.Residenc, r2010 = ob.causas.ext.2010$Obitos_p.Residenc,
+                        r2011 = ob.causas.ext.2011$Obitos_p.Residenc, geral = todos$Obitos_p.Residenc)
+
+ob.causas.ext.ocorr = list(r2007 = ob.causas.ext.2007$Obitos_p.Ocorrenc, r2008 = ob.causas.ext.2008$Obitos_p.Ocorrenc,
+                        r2009 = ob.causas.ext.2009$Obitos_p.Ocorrenc, r2010 = ob.causas.ext.2010$Obitos_p.Ocorrenc,
+                        r2011 = ob.causas.ext.2011$Obitos_p.Ocorrenc, geral = todos$Obitos_p.Ocorrenc)
+
+boxplot(ob.causas.ext.resid, outline=F, main = "Óbitos por residência(sem outliers)")
+boxplot(ob.causas.ext.resid, main = "Óbitos por residência")
+
+boxplot(ob.causas.ext.ocorr, outline=F, main = "Óbitos por ocorrência(sem outliers)")
+boxplot(ob.causas.ext.ocorr, main = "Óbitos por ocorrência")
+
+par(mfrow=c(3,2))
+
+#residencia da mae
+qqnorm(ob.causas.ext.2007$Obitos_p.Residenc, main = "Óbitos por residência - 2007")
+qqnorm(ob.causas.ext.2008$Obitos_p.Residenc, main = "Óbitos por residência - 2008")
+qqnorm(ob.causas.ext.2009$Obitos_p.Residenc, main = "Óbitos por residência - 2009")
+qqnorm(ob.causas.ext.2010$Obitos_p.Residenc, main = "Óbitos por residência - 2010")
+qqnorm(ob.causas.ext.2011$Obitos_p.Residenc, main = "Óbitos por residência - 2011")
+qqnorm(todos$Obitos_p.Residenc, main = "Óbitos por residência - geral")
+
+#ocorrencia
+qqnorm(ob.causas.ext.2007$Obitos_p.Ocorrenc, main = "Óbitos por ocorrência - 2007")
+qqnorm(ob.causas.ext.2008$Obitos_p.Ocorrenc, main = "Óbitos por ocorrência - 2008")
+qqnorm(ob.causas.ext.2009$Obitos_p.Ocorrenc, main = "Óbitos por ocorrência - 2009")
+qqnorm(ob.causas.ext.2010$Obitos_p.Ocorrenc, main = "Óbitos por ocorrência - 2010")
+qqnorm(ob.causas.ext.2011$Obitos_p.Ocorrenc, main = "Óbitos por ocorrência - 2011")
+qqnorm(todos$Obitos_p.Ocorrenc, main = "Óbitos por ocorrência - geral")
+
+####################################
+
+
 a <- formatar.df(ob.causas.ext.2007, "Obitos_p.Residenc", "Obitos_p.Ocorrenc")
