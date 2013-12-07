a951f93f4a0fd5d73551e118c514a36568b2499d
Ricardo Araújo <phd.ricardo@gmail.com>
2013-12-06 21:44:08 -0300

    bar chart com cor cinza certo

--- src/javascript/barchart.js
+++ src/javascript/barchart.js
@@ -11,8 +11,7 @@ function plot_barra_indicador(cidade, indicador_nome) {
     });
   }
 }
-var bounds;
-var estado_faixas;
+
 function plot_bars(data, cidade_nome, indicador){
 
 	var h1 = 60;
@@ -22,13 +21,11 @@ function plot_bars(data, cidade_nome, indicador){
 
 	var current_year = getCurrentYearNotNA(data, cidade_nome, indicador);
 	
-	var rawdata = data.filter(function(d){return d.ANO == current_year;});
-
-	bounds = desvios.filter(function(d){return d.indicador == indicador && d.ano == current_year})[0]
+	var bounds = desvios.filter(function(d){return d.indicador == indicador && d.ano == current_year})[0]
 					.bounds
 					.map(parseFloat);
 	
-	var estado = rawdata;
+	var estado = data.filter(function(d){return d.ANO == current_year;});
 	
 	var cidade = estado.filter(function(d){return d.NOME_MUNICIPIO == cidade_nome})[0];
 
@@ -36,26 +33,23 @@ function plot_bars(data, cidade_nome, indicador){
 
 	var micro = rawdata.filter(function(d){return d.NOME_MICRO == cidade.NOME_MICRO;});
 
-	estado_faixas = bounds.map(function(d){ return {'x': d, 'y': h1};}).sort(function(a, b){ 
+	var estado_faixas = bounds.map(function(d){ return {'x': d, 'y': h1};}).sort(function(a, b){ 
 		return d3.descending(a.x, b.x); 
 	});;
 					
 	var meso_faixas =[
 	                  {'x': (d3.max(estado,function(d){return parseFloat(d[indicador]);})), 'y' : h2},
 	                  {'x': (d3.max(meso,function(d){return parseFloat(d[indicador]);})), 'y' : h2},
-	                {'x' : d3.min(meso,function(d){return parseFloat(d[indicador]);}) , 'y' : h2}, 
-	                {'x' : d3.min(estado,function(d){return parseFloat(d[indicador]);}) , 'y' : h2}
+	               	  {'x' : d3.min(meso,function(d){return parseFloat(d[indicador]);}) , 'y' : h2}, 
+	                  {'x' : d3.min(estado,function(d){return parseFloat(d[indicador]);}) , 'y' : h2}
 					];
 		
 	var micro_faixas = [
-	                    {'x': (d3.max(estado,function(d){return parseFloat(d[indicador]);})), 'y' : h2},
+	                    {'x': (d3.max(estado,function(d){return parseFloat(d[indicador]);})), 'y' : h3},
 	                    {'x': (d3.max(micro,function(d){return parseFloat(d[indicador]);})), 'y' : h3},
     					{'x' :d3.min(micro,function(d){return parseFloat(d[indicador]);}) , 'y' : h3},
-						{'x' : d3.min(estado,function(d){return parseFloat(d[indicador]);}) , 'y' : h2}
+						{'x' : d3.min(estado,function(d){return parseFloat(d[indicador]);}) , 'y' : h3}
 						];
-	
-	
-	
 	
 	var width = 800;
 	var height = 300;
@@ -67,25 +61,31 @@ function plot_bars(data, cidade_nome, indicador){
 		.attr('width', width)
 		.attr('height', height);
 	
-	plot_bar(svg, estado, estado_faixas, x_start, x_end);
-	
-	//plot_bar(svg, meso, meso_faixas, x_start, x_end);
-	//plot_bar(svg, micro, micro_faixas, x_start, x_end);
+	plot_bar(svg, estado, estado_faixas, x_start, x_end, color_scale_indicador);
+	//plot_bar(svg, meso, meso_faixas, x_start, x_end, color_scale_meso);
+	//plot_bar(svg, micro, micro_faixas, x_start, x_end, color_scale_meso);
 
 }
 
-var cinza = "#F0F0F0";
-
-var color_scale_faixa = [
+var color_scale_indicador = [
 	"#006400",
 	"#92B879", 
 	"#FFFF00", 
 	"#FF7F00", 
-	"#F0F0F0", 
-	"#FF0000" 
+	"#E0E0E0", 
+	"#FF0000",
+	"#F0F0F0" 
 ];
 
-function plot_bar(svg_element, cidades, faixas, x_start, x_end){
+var color_scale_meso = [
+	"#F0F0F0",
+	"#E0E0E0",
+	"#F0F0F0"
+];
+
+
+
+function plot_bar(svg_element, cidades, faixas, x_start, x_end, color_scale){
 	
 	var min_x = faixas[faixas.length-1].x;
 	var max_x = faixas[0].x;
@@ -105,27 +105,6 @@ function plot_bar(svg_element, cidades, faixas, x_start, x_end){
 	
 	var line_bar = svg_element.append("g").attr("id", "estado_bar");
 	
-//	line_bar.append("line")
-//	.attr("x1", x_scale(min_x))
-//	.attr("x2", x_scale(max_x))
-//	.attr("y1",y_default)
-//	.attr("y2",y_default)
-//	.attr("id","barra_indicador_altura_" + y_default)
-//	.style("stroke",cinza)
-//	//.attr("opacity",0.2)
-//	.attr("stroke-width",25);
-//	
-//	line_bar.append("line")
-//	.attr("x1", x_scale(min_x))
-//	.attr("x2", x_scale(faixas[faixas.length-2].x))
-//	.attr("y1",y_default)
-//	.attr("y2",y_default)
-//	.attr("id","barra_indicador_altura_" + y_default)
-//	.style("stroke","green")
-//	//.attr("opacity",0.2)
-//	.attr("stroke-width",25);
-	
-	
 	var lines = line_bar.selectAll("line")
 						.data(faixas)
 						.enter()
@@ -135,7 +114,7 @@ function plot_bar(svg_element, cidades, faixas, x_start, x_end){
 						.attr("y1",y_default)
 						.attr("y2",y_default)
 						.attr("id","barra_indicador_altura_" + y_default)
-						.style("stroke",function(d,i){return color_scale_faixa[i]})
+						.style("stroke",function(d,i){return color_scale[i]})
 						//.attr("opacity",0.2)
 						.attr("stroke-width",25)
 	
